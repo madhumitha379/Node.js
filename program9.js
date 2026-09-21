33624cc915a9f0399e0fee20cae0e80d26ce0a5a
madhumitha379 <madhumithad60@gmail.com>
2026-09-21 20:25:35 +0530

    Fix mongoose connection and schema definition

--- program9.js
+++ program9.js
@@ -1,36 +1,30 @@
-const mongoose = require(&quot;mongoose&quot;);
+const mongoose = require("mongoose");
 async function main() {
-await
-mongoose.connect(&quot;mongodb://127.0.0.1:2
-7017/testdb&quot;);
-console.log(&quot;Connected&quot;);
+    await mongoose.connect("mongodb://127.0.0.1:27017/testdb");
+    console.log("Connected");
+    const schema = new mongoose.Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            match: /.+@.+\..+/
+        }
+    });
+    const Model = mongoose.model("Test", schema);
+    const data = new Model({
+        name: "Madhumitha",
+        email: "sk@gmail.com"
+    });
+    try {
+        await data.save();
+        console.log("Data saved successfully");
+    } catch (err) {
+        console.log(err.message);
+    }
 
-const schema = new
-mongoose.Schema({
-name: { type: String, required:
-true },
-email: {
-type: String,
-match: /.+@.+\..+/
-}
-});
-
-const Model =
-mongoose.model(&quot;Test&quot;, schema);
-
-const data = new Model({
-name: &quot;&quot;,
-email: &quot;sk@gmail.com&quot;
-});
-
-try {
-await data.save();
-
-} catch (err) {
-console.log(err.message);
-}
-
-await mongoose.disconnect();
+    await mongoose.disconnect();
 }
 
 main().catch(console.error);
